28bfbe09299ea48972b1be9c2d76fca31796c4bf
ahmed_attia <ahmed.attia@dscoola.com>
2022-11-25 10:21:50 +0200

    feat: add sandbox folder that contain all the basic exercise files

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -3,7 +3,7 @@
 define("DB_SERVER", "localhost");
 // define("DB_USER", "webuser");
 define("DB_USER", "root");
-define("DB_PASS", "mysqlroot");
+define("DB_PASS", "root");
 // define("DB_PASS", "secretpassword");
 define("DB_NAME", "be_coder");
 
